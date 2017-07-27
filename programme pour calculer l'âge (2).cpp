6ed7452e9f4f87a816d3904aab88979d4126116e
LucienWETTA-SELL <shorf213@gmail.com>
2017-07-27 11:44:02 +0200

    ajout "ans"

---
+++ programme pour calculer l'âge (2).cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+cout << "Nous sommes en 2017 quel age avez vous ?" << endl;
+int agePresent(0);
+cin >> agePresent;
+cout << "En 2017 vous avez " << agePresent << " ans !" << endl;
+cout << "Ecrivez une annee pour connaitre l age que vous aurez alors" << endl;
+int anneeFutur(0), ageFutur(0);
+cin >> anneeFutur;
+ageFutur = agePresent + (anneeFutur-2017);
+cout << "En " << anneeFutur << " vous aurez " << ageFutur << " ans" << endl;
+
+
+return 0;
+}
